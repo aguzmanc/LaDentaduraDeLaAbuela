668889cf017591722e3f83f3f2e968f03eea09f4
TheFrankemon <francomontiel@hotmail.com>
2018-01-20 10:38:11 -0400

    Created basic powerup scripts (Only Hop is working)

---
+++ Source/Assets/Scripts/HopController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HopController : MonoBehaviour {
+
+	public int ZHopDistance = 0;
+
+	void OnTriggerEnter (Collider col) {
+		this.gameObject.SetActive (false);
+		col.gameObject.transform.Translate (0, 0, ZHopDistance, Space.World);
+	}
+
+	void Update () {
+		this.transform.Rotate (0, 1, 0);
+	}
+}
